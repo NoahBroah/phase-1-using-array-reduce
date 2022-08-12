67bbe66cb18df6814fbd467d3e968b3896a31cd2
Noah King <noahking123@yahoo.com>
2022-08-12 10:46:25 -0400

    commit tree 6a7fdf8d4258892e79234819b092617feef30a15

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const initialValue = 0
+const totalBatteries = batteryBatches.reduce((batteries,reducer) => batteries + reducer)
 
-// Code your solution here
+console.log(totalBatteries)
